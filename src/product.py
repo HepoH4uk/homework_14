f592d537dc32ee5567a957c4dc5854d6b6ea14a3
AlTim <aleks00_777@mail.ru>
2025-03-11 21:39:46 +0300

    fix product.py

--- src/product.py
+++ src/product.py
@@ -7,7 +7,7 @@ class Product:
     def __init__(self, name, description, price, quantity):
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
 
     @classmethod
@@ -16,3 +16,14 @@ class Product:
                    description=prod_data.get("description"),
                    price=prod_data.get("price"),
                    quantity=prod_data.get("quantity"))
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, new_price):
+        if new_price >= 0:
+            self.__price = new_price
+        else:
+            print("Цена не должна быть нулевая или отрицательная")
