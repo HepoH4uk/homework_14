913b0b0d1ad1976d4e718a6647e0442b6e500cf6
AlTim <aleks00_777@mail.ru>
2025-03-12 18:56:13 +0300

    update test_product.py

--- tests/test_product.py
+++ tests/test_product.py
@@ -1,6 +1,30 @@
+from src.product import Product
 
-def test_products(first_product):
+
+def test_products_1(first_product):
     assert first_product.name == "Samsung Galaxy S23 Ultra"
     assert first_product.description == "256GB, Серый цвет, 200MP камера"
     assert first_product.price == 180000.0
     assert first_product.quantity == 5
+
+
+def test_price_property(first_product):
+    assert first_product.price == 180000.0
+
+
+def test_price_setter(capsys, first_product):
+    first_product.price = -5000
+    message = capsys.readouterr()
+    assert message.out.strip() == "Цена не должна быть нулевая или отрицательная"
+    first_product.price = 0
+    assert first_product.price == 0
+
+
+def test_new_product():
+    new_product = Product.new_product(
+        {"name": "Samsung Galaxy S23 Ultra", "description": "256GB, Серый цвет, 200MP камера", "price": 180000.0,
+         "quantity": 5})
+    assert new_product.name == "Samsung Galaxy S23 Ultra"
+    assert new_product.description == "256GB, Серый цвет, 200MP камера"
+    assert new_product.price == 180000.0
+    assert new_product.quantity == 5
