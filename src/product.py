da20b12a3ea095d8cc5adf1634a266a6c7f544da
HepoH4uk <aleks00_777@mail.ru>
2025-03-15 14:21:49 +0300

    Revert "Feature/task 02"

--- src/product.py
+++ src/product.py
@@ -7,20 +7,5 @@ class Product:
     def __init__(self, name, description, price, quantity):
         self.name = name
         self.description = description
-        self.__price = price
+        self.price = price
         self.quantity = quantity
-
-    @classmethod
-    def new_product(cls, prod_data):
-        return cls(**prod_data)
-
-    @property
-    def price(self):
-        return self.__price
-
-    @price.setter
-    def price(self, new_price):
-        if new_price > 0:
-            self.__price = new_price
-        else:
-            print("Цена не должна быть нулевая или отрицательная")
