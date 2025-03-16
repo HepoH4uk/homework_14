6e884873de50774b35b3324f27250848b27dd94e
AlTim <aleks00_777@mail.ru>
2025-03-16 18:35:07 +0300

    update category.py

--- src/category.py
+++ src/category.py
@@ -15,31 +15,20 @@ class Category:
         Category.category_count += 1
         Category.product_count += len(products) if products else 0
 
+    def __str__(self):
+        product_quantity = 0
+        for product in self.__products:
+            product_quantity += product.quantity
+
+        return f"{self.name}, количество продуктов: {product_quantity}"
+
     @property
     def products(self):
         products_info = []
         for product in self.__products:
-            products_info.append(f"{product.name}, {product.price}, Остаток: {product.quantity} шт.")
+            products_info.append(f"{product.name}, {product.price} руб., Остаток: {product.quantity} шт.")
         return '\n'.join(products_info)
 
     def add_product(self, product: Product):
         self.__products.append(product)
         Category.product_count += 1
-
-
-if __name__ == "__main__":
-    product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
-    product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
-    product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
-
-    category1 = Category(
-        "Смартфоны",
-        "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
-        [product1, product2, product3]
-    )
-
-    print(category1.products)
-    product4 = Product("55\" QLED 4K", "Фоновая подсветка", 123000.0, 7)
-    category1.add_product(product4)
-    print(category1.products)
-    print(category1.product_count)
