89d90fbf8e502b78dc1013ed786dce6e8c8e0567
AlTim <aleks00_777@mail.ru>
2025-03-21 21:00:17 +0300

    update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,8 @@
 import pytest
 
 from src.product import Product
+from src.product import Smartphone
+from src.product import LawnGrass
 from src.category import Category
 
 
@@ -46,3 +48,57 @@ def empty_list_product():
                     description="Современный телевизор, который позволяет наслаждаться просмотром",
                     products=[]
                     )
+
+
+@pytest.fixture
+def first_smartphone():
+    return Smartphone(
+        name="Samsung Galaxy S23 Ultra",
+        description="256GB, Серый цвет, 200MP камера",
+        price=180000.0,
+        quantity=5,
+        efficiency=95.5,
+        model="S23 Ultra",
+        memory=256,
+        color="Серый"
+    )
+
+
+@pytest.fixture
+def second_smartphone():
+    return Smartphone(
+        name="Iphone 15",
+        description="512GB, Gray space",
+        price=210000.0,
+        quantity=8,
+        efficiency=98.2,
+        model="15",
+        memory=512,
+        color="Gray space"
+    )
+
+
+@pytest.fixture
+def first_grass():
+    return LawnGrass(
+        name="Газонная трава",
+        description="Элитная трава для газона",
+        price=500.0,
+        quantity=20,
+        country="Россия",
+        germination_period="7 дней",
+        color="Зеленый"
+    )
+
+
+@pytest.fixture
+def second_grass():
+    return LawnGrass(
+        name="Газонная трава 2",
+        description="Выносливая трава",
+        price=450.0,
+        quantity=15,
+        country="США",
+        germination_period="5 дней",
+        color="Темно-зеленый"
+    )
